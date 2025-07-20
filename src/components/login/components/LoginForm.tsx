9ede33989ec6b2af61fe6b252852696dcb6578c7
shivkanpur <shiv.kanpur@gmail.com>
2025-07-21 00:08:16 +0530

    no message

--- src/components/login/components/LoginForm.tsx
+++ src/components/login/components/LoginForm.tsx
@@ -128,7 +128,8 @@ const LoginForm = () => {
 
       if (result.success && result.user) {
         if (result.token) {
-          AccountService.storeAuthData(result.token, result.user);
+            const rememberMe = true; // <-- Get this from UI if needed
+    AccountService.storeAuthData(result.token, result.user, rememberMe);                
         }
         const planId = location.state?.planId;
         const selectedPlan = location.state?.selectedPlan;
@@ -158,6 +159,8 @@ const LoginForm = () => {
       setIsLoading(false);
     }
   };
+
+  
 
 
   return (
