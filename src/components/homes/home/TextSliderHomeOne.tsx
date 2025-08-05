bfeb577b0df6391ab5aa200332a7ffed9fc66dc6
GlennColegate <glenn@enigmainc.co.uk>
2025-08-05 10:01:48 +0100

    Text slider update

--- src/components/homes/home/TextSliderHomeOne.tsx
+++ src/components/homes/home/TextSliderHomeOne.tsx
@@ -8,16 +8,6 @@ const TextSliderHomeOne = () => {
               <div className="scroller-x">
                 <ul className="list list-row scroller-x__list gap-6">
                   <li>
-                    <h2 className="text-light">Generator Of easy install</h2>
-                  </li>
-                  <li>
-                    <img
-                      src="assets/img/icon-star.png"
-                      alt="image"
-                      className="img-fluid"
-                    />
-                  </li>
-                  <li>
                     <h2 className="text-outline">Good Game</h2>
                   </li>
                   <li>
@@ -36,19 +26,6 @@ const TextSliderHomeOne = () => {
                       alt="image"
                       className="img-fluid"
                     />
-                  </li>
-                  <li>
-                    <h2 className="text-outline">Self managing</h2>
-                  </li>
-                  <li>
-                    <img
-                      src="assets/img/icon-star.png"
-                      alt="image"
-                      className="img-fluid"
-                    />
-                  </li>
-                  <li>
-                    <h2 className="text-light">Generator Of Chatbots</h2>
                   </li>
                 </ul>
               </div>
