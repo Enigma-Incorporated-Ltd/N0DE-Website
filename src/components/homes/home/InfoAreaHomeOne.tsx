864f408dd0e7ce977d55d29b9f4521973102191b
nikolvos <nikolacvetkovic12@gmail.com>
2025-12-12 14:24:52 +0100

    UI Fixes in the footer and more. Added a new InfoAreaHomeOne component.

--- src/components/homes/home/InfoAreaHomeOne.tsx
+++ src/components/homes/home/InfoAreaHomeOne.tsx
@@ -1,84 +1,136 @@
+import ryanPhoto from "/assets/ryan/profile.jpg";
+import leveledUp from "/assets/img_partner/logo-white.png";
+import iconPlaceholder from "/assets/img/placeholder-img.png";
+
+const stats = [
+  {
+    title: "jitter reduction",
+    percentage: 95,
+    img: "/assets/ryan/jitter.png",
+  },
+  {
+    title: "faster downloads",
+    percentage: 210,
+    img: "/assets/ryan/download.png",
+  },
+  {
+    title: "more content output",
+    percentage: 40,
+    img: "/assets/ryan/content output.png",
+  },
+  {
+    title: "[GMC: unknown]",
+    percentage: 60,
+    img: "/assets/ryan/jitter.png",
+  },
+];
+
 const InfoAreaHomeOne = () => {
   return (
-    <>
-      <div className="section-space-md-y info-section">
-        <div className="container">
-          <div className="row g-4 align-items-center">
-            <div className="col-lg-5">
-              <div className="pe-xl-12" data-cue="slideInUp">
-                <img
-                  src="/assets/img/info-section-img.png"
-                  alt="image"
-                  className="img-fluid"
-                />
-              </div>
+    <div className="section-space-md-y info-section">
+      <div className="container">
+        <div className="row g-5">
+          {/* Top Header / Quotes */}
+          <div className="row g-4 align-items-center mb-10">
+            <div
+              className="d-inline-flex align-items-center flex-wrap row-gap-2 column-gap-4"
+              data-cue="fadeIn"
+            >
+              <div className="flex-shrink-0 d-inline-block w-20 h-2px bg-primary-gradient"></div>
+              <span className="d-block fw-medium text-light fs-20">
+                N0DE: Dramatic Real World Gains for Gamers and Streamers
+              </span>
             </div>
-            <div className="col-lg-7">
-              <h2 className="h4 text-light mb-6" data-cue="fadeIn">
-                ''N0DE Pro helps me stream and play etc...''
-              </h2>
-              <div
-                className="d-flex align-items-center gap-5"
-                data-cue="fadeIn"
-              >
-                <div className="d-grid place-content-center w-15 h-15 rounded-circle overflow-hidden flex-shrink-0">
-                  <img
-                    src="/assets/img_partner/logo-white.png"
-                    alt="image"
-                    className="w-100 h-100 object-fit-cover"
-                  />
-                </div>
-                <div className="flex-grow-1">
-                  <h6 className="mb-0 text-light">Ryan Sparrowhawk</h6>
-                  <span className="d-block fs-14 text-light text-opacity-50">
+
+            <span className="d-block fw-semibold text-light fs-32">
+              "N0DE genuinely improdved my online experience"
+            </span>
+            <span className="d-block fw-semibold text-light fs-32">
+              "I highly recommend N0DE"
+            </span>
+            <span className="d-block fw-light text-light fs-22">
+              "I'm still on copper broadband with just 54Mbps download, which
+              struggles with multi-streaming.
+            </span>
+            <span className="d-block fw-light text-light fs-22">
+              After switching to N0DE, I saw a noticable drop in packet loss and
+              a much smoother ping.."
+            </span>
+          </div>
+
+          <div className="col-12 d-flex flex-column flex-md-row align-items-start gap-5 ">
+            <div className="d-flex flex-column " style={{ maxWidth: "320px" }}>
+              <img
+                src={ryanPhoto}
+                alt="Ryan Sparrowhawk"
+                className="img-fluid rounded mb-3 p-8"
+              />
+
+              <div className="d-flex align-items-center mb-3">
+                <img
+                  src={leveledUp}
+                  alt="N0DE"
+                  className="me-3"
+                  style={{ width: "40px" }}
+                />
+
+                <div>
+                  <span className="d-block fw-semibold text-light fs-24">
+                    Ryan Sparrowhawk
+                  </span>
+                  <span className="d-block fw-light fs-14">
                     Levelled Up Gaming
                   </span>
                 </div>
               </div>
-              <div
-                className="bg-dark-gradient p-4 p-sm-6 p-md-10 p-lg-6 p-xl-8 p-xxl-10 rounded-5"
-                data-cue="fadeIn"
-              >
-                <ul className="list list-row flex-wrap info-list">
-                  <li>
-                    <div className="d-flex align-items-center gap-6 p-4 p-md-6">
-                      <h3 className="mb-0 text-light flex-shrink-0">95%</h3>
-                      <p className="mb-0 text-opacity-50 flex-grow-1">
-                        reduction in jitter
-                      </p>
+
+              <p className="text-light fs-20 fw-light">
+                Levelled Up Gaming is a thriving community with over 500,000
+                followers across all platforms. As an early adopter of N0DE,
+                Ryan has experienced significant performance gains across his
+                diverse networking needs.
+              </p>
+            </div>
+            {/* 
+            <div className="process-card rounded-5 p-8 h-100 w-100 d-flex flex-column stats-grid">
+              <div className="row row-cols-1 row-cols-sm-2 g-4 h-100 w-100">
+                {stats.map((s, i) => (
+                  <div className="col" key={i}>
+                    <div className="h-100 d-flex flex-column align-items-center">
+                      <div className="stat-img">
+                        <img src={s.img} alt="icon" className="w-100 h-100" />
+                      </div>
+
+                      <span className="text-gradient-primary h3 fw-bold fs-32 mb-1">
+                        {s.percentage}%
+                      </span>
+                      <span className="text-light fs-16">{s.title}</span>
                     </div>
-                  </li>
-                  <li>
-                    <div className="d-flex align-items-center gap-6 p-4 p-md-6">
-                      <h3 className="mb-0 text-light flex-shrink-0">210%</h3>
-                      <p className="mb-0 text-opacity-50 flex-grow-1">
-                        faster downloads
-                      </p>
+                  </div>
+                ))}
+              </div>
+            </div> */}
+            <div className="process-card rounded-5 p-8 h-100 w-100 d-flex flex-column stats-grid">
+              <div className="row row-cols-1 row-cols-sm-2 g-4 h-100 w-100 stats-divider">
+                {stats.map((s, i) => (
+                  <div className="col" key={i}>
+                    <div className="h-100 d-flex flex-column align-items-center">
+                      <div className="stat-img">
+                        <img src={s.img} alt="icon" className="w-100 h-100" />
+                      </div>
+                      <span className="text-gradient-primary h3 fw-bold fs-32 mb-1">
+                        {s.percentage}%
+                      </span>
+                      <span className="text-light fs-16">{s.title}</span>
                     </div>
-                  </li>
-                  <li>
-                    <div className="d-flex align-items-center gap-6 p-4 p-md-6">
-                      <h3 className="mb-0 text-light flex-shrink-0">40%</h3>
-                      <p className="mb-0 text-opacity-50 flex-grow-1">
-                        increase in content output
-                      </p>
-                    </div>
-                  </li>
-                  <li>
-                    <div className="d-flex align-items-center gap-6 p-4 p-md-6">
-                      <h3 className="mb-0 text-light flex-shrink-0">60%</h3>
-                      <p className="mb-0 text-opacity-50 flex-grow-1">
-                        increase in content output
-                      </p>
-                    </div>
-                  </li>
-                </ul>
+                  </div>
+                ))}
               </div>
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/layouts/footers/FooterOne.tsx
+++ src/layouts/footers/FooterOne.tsx
@@ -7,7 +7,7 @@ interface FooterOneProps {
 const FooterOne = ({ style_2 }: FooterOneProps) => {
   return (
     <footer className={`${style_2 ? "footer-2" : "footer-1"} overflow-hidden`}>
-      <div className="container px-4">
+      <div className="container px-4 mt-5">
         <div className="row gy-4 justify-content-center">
           <div className="col-12 col-md-6 col-xl-4">
             <Link to="/" className="logo d-block mb-4">
@@ -35,49 +35,42 @@ const FooterOne = ({ style_2 }: FooterOneProps) => {
               </button>
             </div>
           </div>
-          
+
           <div className="col-12 col-md-6 col-xl-8">
             <div className="row gy-4">
               <div className="col-6 col-sm-6 col-md-3">
                 <h5 className="text-light mb-4">Company</h5>
                 <ul className="list-unstyled mb-0">
-                  {/* <li className="mb-2">
-                    <Link to="/about" className="text-light text-opacity-70 text-decoration-none">
-                      About Us
-                    </Link>
-                  </li> */}
                   <li className="mb-2">
-                    <Link to="/service" className="text-light text-opacity-70 text-decoration-none">
+                    <Link
+                      to="/service"
+                      className="text-light text-opacity-70 text-decoration-none"
+                    >
                       Our Mission
                     </Link>
                   </li>
-                  {/*<li className="mb-2">*/}
-                  {/*  <Link to="/about" className="text-light text-opacity-70 text-decoration-none">*/}
-                  {/*    Company History*/}
-                  {/*  </Link>*/}
-                  {/*</li>*/}
-                  {/*<li className="mb-2">*/}
-                  {/*  <Link to="/about" className="text-light text-opacity-70 text-decoration-none">*/}
-                  {/*    Testimonials*/}
-                  {/*  </Link>*/}
-                  {/*</li>*/}
-                  {/*<li className="mb-2">*/}
-                  {/*  <Link to="/contact" className="text-light text-opacity-70 text-decoration-none">*/}
-                  {/*    Careers*/}
-                  {/*  </Link>*/}
-                  {/*</li>*/}
+
                   <li className="mb-2">
-                    <Link to="/about" className="text-light text-opacity-70 text-decoration-none">
+                    <Link
+                      to="/about"
+                      className="text-light text-opacity-70 text-decoration-none"
+                    >
                       Company History
                     </Link>
                   </li>
                   <li className="mb-2">
-                    <Link to="/about" className="text-light text-opacity-70 text-decoration-none">
+                    <Link
+                      to="/about"
+                      className="text-light text-opacity-70 text-decoration-none"
+                    >
                       Testimonials
                     </Link>
                   </li>
                   <li className="mb-2">
-                    <Link to="/contact" className="text-light text-opacity-70 text-decoration-none">
+                    <Link
+                      to="/contact"
+                      className="text-light text-opacity-70 text-decoration-none"
+                    >
                       Careers
                     </Link>
                   </li>
@@ -88,88 +81,88 @@ const FooterOne = ({ style_2 }: FooterOneProps) => {
                 <h5 className="text-light mb-4">Support</h5>
                 <ul className="list-unstyled mb-0">
                   <li className="mb-2">
-                    <Link to="/contact" className="text-light text-opacity-70 text-decoration-none">
+                    <Link
+                      to="/contact"
+                      className="text-light text-opacity-70 text-decoration-none"
+                    >
                       Contact Us
                     </Link>
                   </li>
                   <li className="mb-2">
-                    <Link to="/about" className="text-light text-opacity-70 text-decoration-none">
+                    <Link
+                      to="/about"
+                      className="text-light text-opacity-70 text-decoration-none"
+                    >
                       Privacy Policy
                     </Link>
                   </li>
                   <li className="mb-2">
-                    <Link to="/about" className="text-light text-opacity-70 text-decoration-none">
+                    <Link
+                      to="/legal"
+                      className="text-light text-opacity-70 text-decoration-none"
+                    >
                       Terms & Conditions
                     </Link>
                   </li>
                   <li className="mb-2">
-                    <Link to="/about" className="text-light text-opacity-70 text-decoration-none">
+                    <Link
+                      to="/about"
+                      className="text-light text-opacity-70 text-decoration-none"
+                    >
                       Cookies
                     </Link>
                   </li>
                   <li className="mb-2">
-                    <Link to="/faq" className="text-light text-opacity-70 text-decoration-none">
+                    <Link
+                      to="/faq"
+                      className="text-light text-opacity-70 text-decoration-none"
+                    >
                       FAQ
                     </Link>
                   </li>
                 </ul>
               </div>
 
-              {/*<div className="col-6 col-sm-6 col-md-3">*/}
-              {/*  <h5 className="text-light mb-4">Product</h5>*/}
-              {/*  <ul className="list-unstyled mb-0">*/}
-              {/*    <li className="mb-2">*/}
-              {/*      <Link to="/blog-list" className="text-light text-opacity-70 text-decoration-none">*/}
-              {/*        Recents*/}
-              {/*      </Link>*/}
-              {/*    </li>*/}
-              {/*    <li className="mb-2">*/}
-              {/*      <Link to="/blog" className="text-light text-opacity-70 text-decoration-none">*/}
-              {/*        Upcoming*/}
-              {/*      </Link>*/}
-              {/*    </li>*/}
-              {/*    <li className="mb-2">*/}
-              {/*      <Link to="/service" className="text-light text-opacity-70 text-decoration-none">*/}
-              {/*        Builder*/}
-              {/*      </Link>*/}
-              {/*    </li>*/}
-              {/*    <li className="mb-2">*/}
-              {/*      <Link to="/about" className="text-light text-opacity-70 text-decoration-none">*/}
-              {/*        On Sale*/}
-              {/*      </Link>*/}
-              {/*    </li>*/}
-              {/*    <li className="mb-2">*/}
-              {/*      <Link to="/contact" className="text-light text-opacity-70 text-decoration-none">*/}
-              {/*        Live Demo*/}
-              {/*      </Link>*/}
-              {/*    </li>*/}
-              {/*  </ul>*/}
-              {/*</div>*/}
               <div className="col-6 col-sm-6 col-md-3">
                 <h5 className="text-light mb-4">Product</h5>
                 <ul className="list-unstyled mb-0">
                   <li className="mb-2">
-                    <Link to="/blog-list" className="text-light text-opacity-70 text-decoration-none">
+                    <Link
+                      to="/blog-list"
+                      className="text-light text-opacity-70 text-decoration-none"
+                    >
                       Recents
                     </Link>
                   </li>
                   <li className="mb-2">
-                    <Link to="/blog" className="text-light text-opacity-70 text-decoration-none">
+                    <Link
+                      to="/blog"
+                      className="text-light text-opacity-70 text-decoration-none"
+                    >
                       Upcoming
                     </Link>
                   </li>
                   <li className="mb-2">
-                    <Link to="/service" className="text-light text-opacity-70 text-decoration-none">
+                    <Link
+                      to="/service"
+                      className="text-light text-opacity-70 text-decoration-none"
+                    >
                       Builder
                     </Link>
                   </li>
                   <li className="mb-2">
-                    <Link to="/about" className="text-light text-opacity-70 text-decoration-none">
+                    <Link
+                      to="/about"
+                      className="text-light text-opacity-70 text-decoration-none"
+                    >
                       On Sale
                     </Link>
                   </li>
                   <li className="mb-2">
-                    <Link to="/contact" className="text-light text-opacity-70 text-decoration-none">
+                    <Link
+                      to="/contact"
+                      className="text-light text-opacity-70 text-decoration-none"
+                    >
                       Live Demo
                     </Link>
                   </li>
@@ -180,27 +173,42 @@ const FooterOne = ({ style_2 }: FooterOneProps) => {
                 <h5 className="text-light mb-4">Follow Us</h5>
                 <ul className="list-unstyled mb-0">
                   <li className="mb-2">
-                    <a href="#" className="text-light text-opacity-70 text-decoration-none">
+                    <a
+                      href="#"
+                      className="text-light text-opacity-70 text-decoration-none"
+                    >
                       TikTok
                     </a>
                   </li>
                   <li className="mb-2">
-                    <a href="#" className="text-light text-opacity-70 text-decoration-none">
+                    <a
+                      href="#"
+                      className="text-light text-opacity-70 text-decoration-none"
+                    >
                       Instagram
                     </a>
                   </li>
                   <li className="mb-2">
-                    <a href="#" className="text-light text-opacity-70 text-decoration-none">
+                    <a
+                      href="#"
+                      className="text-light text-opacity-70 text-decoration-none"
+                    >
                       Discord
                     </a>
                   </li>
                   <li className="mb-2">
-                    <a href="#" className="text-light text-opacity-70 text-decoration-none">
+                    <a
+                      href="#"
+                      className="text-light text-opacity-70 text-decoration-none"
+                    >
                       Twitter
                     </a>
                   </li>
                   <li className="mb-2">
-                    <a href="#" className="text-light text-opacity-70 text-decoration-none">
+                    <a
+                      href="#"
+                      className="text-light text-opacity-70 text-decoration-none"
+                    >
                       Facebook
                     </a>
                   </li>
@@ -216,45 +224,208 @@ const FooterOne = ({ style_2 }: FooterOneProps) => {
           <div className="row py-4 align-items-center">
             <div className="col-12 col-md-6 text-center text-md-start mb-3 mb-md-0">
               <p className="mb-0 text-light text-opacity-70">
-                © {new Date().getFullYear()} N0DE - an Enigma Inc brand. All rights reserved.
+                © {new Date().getFullYear()} N0DE - an Enigma Inc brand. All
+                rights reserved.
               </p>
             </div>
             <div className="col-12 col-md-6 text-center text-md-end">
               <ul className="list-inline mb-0">
                 <li className="list-inline-item">
-                  <a href="#" className="text-light text-opacity-70 text-decoration-none">
+                  <a
+                    href="#"
+                    className="text-light text-opacity-70 text-decoration-none"
+                  >
                     Terms of Service
                   </a>
                 </li>
                 <li className="list-inline-item ms-3">
-                  <a href="#" className="text-light text-opacity-70 text-decoration-none">
+                  <a
+                    href="#"
+                    className="text-light text-opacity-70 text-decoration-none"
+                  >
                     Privacy Policy
                   </a>
                 </li>
                 <li className="list-inline-item ms-3">
-                  <a href="#" className="text-light text-opacity-70 text-decoration-none">
+                  <a
+                    href="#"
+                    className="text-light text-opacity-70 text-decoration-none"
+                  >
                     Cookie Policy
                   </a>
                 </li>
                 <li className="list-inline-item ms-3">
-                  <Link to="/legal" className="text-light text-opacity-70 text-decoration-none">
+                  <Link
+                    to="/legal"
+                    className="text-light text-opacity-70 text-decoration-none"
+                  >
                     Stealth‑Level Security & Privacy
                   </Link>
                 </li>
               </ul>
             </div>
-          </div> 
+          </div>
         </div>
       </div>
 
       {style_2 && (
         <>
-          <img src="assets/img/hero-2-shape-1.png" alt="image" className="img-fluid footer-2__shape-2" />
-          <img src="assets/img/hero-2-shape-2.png" alt="image" className="img-fluid footer-2__shape-1" />
+          <img
+            src="assets/img/hero-2-shape-1.png"
+            alt="image"
+            className="img-fluid footer-2__shape-2"
+          />
+          <img
+            src="assets/img/hero-2-shape-2.png"
+            alt="image"
+            className="img-fluid footer-2__shape-1"
+          />
         </>
       )}
     </footer>
   );
 };
 
-export default FooterOne; 
+export default FooterOne;
+{
+  /* <li className="mb-2">
+                    <Link to="/about" className="text-light text-opacity-70 text-decoration-none">
+                      About Us
+                    </Link>
+                  </li> */
+}
+{
+  /*<li className="mb-2">*/
+}
+{
+  /*  <Link to="/about" className="text-light text-opacity-70 text-decoration-none">*/
+}
+{
+  /*    Company History*/
+}
+{
+  /*  </Link>*/
+}
+{
+  /*</li>*/
+}
+{
+  /*<li className="mb-2">*/
+}
+{
+  /*  <Link to="/about" className="text-light text-opacity-70 text-decoration-none">*/
+}
+{
+  /*    Testimonials*/
+}
+{
+  /*  </Link>*/
+}
+{
+  /*</li>*/
+}
+{
+  /*<li className="mb-2">*/
+}
+{
+  /*  <Link to="/contact" className="text-light text-opacity-70 text-decoration-none">*/
+}
+{
+  /*    Careers*/
+}
+{
+  /*  </Link>*/
+}
+{
+  /*</li>*/
+}
+{
+  /*<div className="col-6 col-sm-6 col-md-3">*/
+}
+{
+  /*  <h5 className="text-light mb-4">Product</h5>*/
+}
+{
+  /*  <ul className="list-unstyled mb-0">*/
+}
+{
+  /*    <li className="mb-2">*/
+}
+{
+  /*      <Link to="/blog-list" className="text-light text-opacity-70 text-decoration-none">*/
+}
+{
+  /*        Recents*/
+}
+{
+  /*      </Link>*/
+}
+{
+  /*    </li>*/
+}
+{
+  /*    <li className="mb-2">*/
+}
+{
+  /*      <Link to="/blog" className="text-light text-opacity-70 text-decoration-none">*/
+}
+{
+  /*        Upcoming*/
+}
+{
+  /*      </Link>*/
+}
+{
+  /*    </li>*/
+}
+{
+  /*    <li className="mb-2">*/
+}
+{
+  /*      <Link to="/service" className="text-light text-opacity-70 text-decoration-none">*/
+}
+{
+  /*        Builder*/
+}
+{
+  /*      </Link>*/
+}
+{
+  /*    </li>*/
+}
+{
+  /*    <li className="mb-2">*/
+}
+{
+  /*      <Link to="/about" className="text-light text-opacity-70 text-decoration-none">*/
+}
+{
+  /*        On Sale*/
+}
+{
+  /*      </Link>*/
+}
+{
+  /*    </li>*/
+}
+{
+  /*    <li className="mb-2">*/
+}
+{
+  /*      <Link to="/contact" className="text-light text-opacity-70 text-decoration-none">*/
+}
+{
+  /*        Live Demo*/
+}
+{
+  /*      </Link>*/
+}
+{
+  /*    </li>*/
+}
+{
+  /*  </ul>*/
+}
+{
+  /*</div>*/
+}
